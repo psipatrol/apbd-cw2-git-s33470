ae7d6afc7c1599676dbe3c1426fde872bb2aad43
psipatrol <s33470@pjwstk.edu.pl>
2026-03-23 15:08:19 +0100

    Projector class crated with its own builder class

---
+++ apbd-cw2-git-s33470/equipment/projector/Projector.cs
@@ -0,0 +1,19 @@
+﻿namespace apbd_cw2_git_s33470.equipment.projector;
+
+public class Projector : AbstractEquipment
+{
+    public string Model {get; init;}
+    public int Lumens {get; init;}
+    
+    public Projector(
+        string name, 
+        double value, 
+        Status status,
+        string model,
+        int lumens
+    ) : base(name, value, status)
+    {
+        Model = model;
+        Lumens = lumens;
+    }
+}
