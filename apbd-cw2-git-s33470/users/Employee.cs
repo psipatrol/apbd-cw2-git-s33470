03f83800171f914bbcb640d2d9905d960ecaa366
psipatrol <s33470@pjwstk.edu.pl>
2026-03-23 15:39:17 +0100

    Employee class created

---
+++ apbd-cw2-git-s33470/users/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace apbd_cw2_git_s33470.users;
+
+public class Employee : AbstractUser
+{
+    public string Department { get; init; }
+    public override int MaxEquipment => 5;
+    public override UserType Type => UserType.Employee;
+    
+    public Employee(
+        string firstName, 
+        string lastName,
+        string department
+        ) : base(firstName, lastName)
+    {
+        Department = department;
+    }
+}
