c48bd97d5f496cbb8e5616c4cabd878a342cf107
psipatrol <s33470@pjwstk.edu.pl>
2026-03-23 15:05:21 +0100

    Camera class crated with its own builder class

---
+++ apbd-cw2-git-s33470/equipment/camera/Camera.cs
@@ -0,0 +1,19 @@
+﻿namespace apbd_cw2_git_s33470.equipment.camera;
+
+public class Camera : AbstractEquipment
+{
+    public string Model {get; init;}
+    public string SensorType {get; init;}
+    
+    public Camera(
+        string name, 
+        double value, 
+        Status status,
+        string model,
+        string sensorType
+        ) : base(name, value, status)
+    {
+        Model = model;
+        SensorType = sensorType;
+    }
+}
