5cbbf330b02de81eb35527655f1b4700edaaa656
psipatrol <s33470@pjwstk.edu.pl>
2026-03-23 15:38:00 +0100

    User abstraction and user type enum created

---
+++ apbd-cw2-git-s33470/users/AbstractUser.cs
@@ -0,0 +1,28 @@
+﻿namespace apbd_cw2_git_s33470.users;
+
+public abstract class AbstractUser
+{
+    private static int _IdCounter = 0;
+    public int Id { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public abstract UserType Type { get; }
+    public abstract int MaxEquipment { get; }
+
+    protected AbstractUser(string firstName, string lastName)
+    {
+        Id = GetId();
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public override string ToString()
+    {
+        return $"({Id}): {FirstName} {LastName}";
+    }
+    
+    private static int GetId()
+    {
+        return _IdCounter++;
+    }
+}
---
+++ apbd-cw2-git-s33470/users/UserType.cs
@@ -0,0 +1,7 @@
+namespace apbd_cw2_git_s33470.users;
+
+public enum UserType
+{
+    Student,
+    Employee
+}
